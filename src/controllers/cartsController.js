896e07e48f8b69c51df46db03306c72b7bc2c7fa
joseperezm <coderhouse@jjpm.cl>
2024-05-22 06:00:18 -0400

    estandarizando estilo de mensajes

--- src/controllers/cartsController.js
+++ src/controllers/cartsController.js
@@ -49,17 +49,17 @@ exports.addToCart = async (req, res, next) => {
 
         if (!product) {
             logger.info("Producto no encontrado");
-            return res.status(404).json({ message: 'Producto no encontrado' });
+            return res.status(404).json({ success: false, message: 'Producto no encontrado' });
         }
 
         if (userRole === 'premium' && product.owner.toString() === userId.toString()) {
             logger.warn(`Usuario premium intentó agregar un producto que le pertenece. User ID: ${userId}, Product Owner: ${product.owner}`);
-            return res.status(403).json({ message: 'No puedes agregar tu propio producto al carrito' });
+            return res.status(403).json({ success: false, message: 'No puedes agregar tu propio producto al carrito' });
         }
 
         const { success, message, cart } = await cartService.addToCart(cid, pid, quantity);
         if (success) {
-            res.json({ message, cart });
+            res.json({ success: true, message, cart });
         } else {
             next({ code: 'NOT_FOUND', message });
         }
